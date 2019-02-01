d003d56e1e363238453f681d171b675bb4f12e26
CoutinhoElias <coutinho.elias@gmail.com>
2019-02-01 18:39:23 -0300

    Configurando app no Heroku.

--- pesquisasatisfacao/settings.py
+++ pesquisasatisfacao/settings.py
@@ -111,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-#LANGUAGE_CODE = 'pt-br'
 LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'America/Fortaleza'
