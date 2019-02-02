471474e5fe7e549e21e5eb1d510a8309cf60d2bc
CoutinhoElias <coutinho.elias@gmail.com>
2019-02-02 06:17:36 -0300

    Ajuste no statifiles.

--- pesquisasatisfacao/settings.py
+++ pesquisasatisfacao/settings.py
@@ -132,8 +132,3 @@ USE_THOUSAND_SEPARATOR = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
